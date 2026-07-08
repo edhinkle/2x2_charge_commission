d3e7c4d5e6ac37d013d1ebde076bbd4e62ec03ad
Enda Mao <edgarmao@login01.chn.perlmutter.nersc.gov>
2026-07-08 09:03:28 -0700

    Trial differential data produced, backup before developing new code.

--- July2026/run_make_2x2_charge_pedestal_WCC_plots.sh
+++ July2026/run_make_2x2_charge_pedestal_WCC_plots.sh
@@ -1,16 +1,30 @@
 #!/usr/bin/env bash
 
 NOM_DSET_IDX=0 # Nominal dataset is index 0 unless otherwise noted
+'''
+DATADIR1="/global/cfs/cdirs/dune/users/edgarmao/trial_pedestal/"
+#DATADIR1="/global/cfs/cdirs/dune/www/data/2x2/nearline/packet/commission/June2024/thresholding_06_07/"
+DATASET1_NAME="trial_pedestal"
+DATE1="2026_07_07"
+DICT1_NAME="/global/cfs/cdirs/dune/users/edgarmao/2x2_crs_analysis/2x2_charge_commission/July2026/channel_dicts/trial_pedestal_2026_07_07_FULL_channel_dict.json"
+'''
+
 DATADIR1="/global/cfs/cdirs/dune/www/data/2x2/CRS.run2/WarmComissioning/Nominal_Pedestal_Correct_Triggering/"
 #DATADIR1="/global/cfs/cdirs/dune/www/data/2x2/nearline/packet/commission/June2024/thresholding_06_07/"
-DATASET1_NAME="Nominal_Warm"
+DATASET1_NAME="Nominal_Pedestal_Correct_Triggering"
 DATE1="2025_08_26"
-DICT1_NAME="channel_dicts/26August2025_Nominal_Pedestal_Periodic_Reset_Enabled_Mod0123_2025_08_26_FULL_channel_dict.json"
+DICT1_NAME="/global/cfs/cdirs/dune/users/edgarmao/2x2_crs_analysis/2x2_charge_commission/July2026/channel_dicts/26August2025_Nominal_Pedestal_Periodic_Reset_Enabled_Mod0123_2025_08_26_FULL_channel_dict.json"
 
-#DATADIR2="/global/cfs/cdirs/dune/www/data/2x2/CRS.run2/WarmComissioning/WholeDetector_20250824_1/"
-#DATASET2_NAME="Periodic_Reset_Disabled_Warm"
-#DATE2="2025_08_24"
-#DICT2_NAME="channel_dicts/24August2025_Nominal_Pedestal_Mod0123_2025_08_24_FULL_channel_dict.json"
+'''
+DATADIR2="/global/cfs/cdirs/dune/users/edgarmao/trial_pedestal2/"
+DATASET2_NAME="trial_pedestal2"
+DATE2="2026_07_07"
+DICT2_NAME="/global/cfs/cdirs/dune/users/edgarmao/2x2_crs_analysis/2x2_charge_commission/July2026/channel_dicts/trial_pedestal2_2026_07_07_FULL_channel_dict.json"
+'''
+DATADIR2="/global/cfs/cdirs/dune/www/data/2x2/CRS.run2/WarmComissioning/WholeDetector_20250822/"
+DATASET2_NAME="WholeDetector"
+DATE2="2025_08_22"
+DICT2_NAME="/global/cfs/cdirs/dune/users/edgarmao/2x2_crs_analysis/2x2_charge_commission/July2026/channel_dicts/22August2025_Nominal_Pedestal_Mod0123_2025_08_22_FULL_channel_dict.json"
 
 #DATADIR3="/global/cfs/cdirs/dune/www/data/2x2/CRS.run2/WarmComissioning/Nominal_Pedestal_256us/"
 #DATASET3_NAME="Periodic_Reset_Frequency_256us_Warm"
@@ -32,30 +46,30 @@ DICT1_NAME="channel_dicts/26August2025_Nominal_Pedestal_Periodic_Reset_Enabled_M
 #DATE6="2025_08_27"
 #DICT6_NAME="channel_dicts/27August2025_Pedestal_Mod0123_LRS_On_Not_Taking_Data_2025_08_27_FULL_channel_dict.json"
 
-DATADIR7="/global/cfs/cdirs/dune/www/data/2x2/CRS.run2/WarmComissioning/differential_LRS_2/"
-DATASET7_NAME="LRS_On_AND_Taking_Data_Warm"
-DATE7="2025_08_28"
-DICT7_NAME="channel_dicts/28August2025_Pedestal_Mod0123_LRS_On_AND_Taking_Data_2025_08_28_FULL_channel_dict.json"
+#DATADIR7="/global/cfs/cdirs/dune/www/data/2x2/CRS.run2/WarmComissioning/differential_LRS_2/"
+#DATASET7_NAME="LRS_On_AND_Taking_Data_Warm"
+#DATE7="2025_08_28"
+#DICT7_NAME="channel_dicts/28August2025_Pedestal_Mod0123_LRS_On_AND_Taking_Data_2025_08_28_FULL_channel_dict.json"
 
-DATADIR8="/global/cfs/cdirs/dune/www/data/2x2/CRS.run2/WarmComissioning/differential_levelMeter/"
-DATASET8_NAME="Level_Probe_On_Warm"
-DATE8="2025_08_28"
-DICT8_NAME="channel_dicts/28August2025_Pedestal_Mod0123_Level_Meter_On_2025_08_28_FULL_channel_dict.json"
+#DATADIR8="/global/cfs/cdirs/dune/www/data/2x2/CRS.run2/WarmComissioning/differential_levelMeter/"
+#DATASET8_NAME="Level_Probe_On_Warm"
+#DATE8="2025_08_28"
+#DICT8_NAME="channel_dicts/28August2025_Pedestal_Mod0123_Level_Meter_On_2025_08_28_FULL_channel_dict.json"
 
-DATADIR9="/global/cfs/cdirs/dune/www/data/2x2/CRS.run2/WarmComissioning/differential_cryoPumpEnabled/"
-DATASET9_NAME="Cryo_Pump_Enabled_But_Off_Warm"
-DATE9="2025_08_29"
-DICT9_NAME="channel_dicts/29August2025_Pedestal_Mod0123_Cryo_Pump_Enabled_NOT_On_2025_08_29_FULL_channel_dict.json"
+#DATADIR9="/global/cfs/cdirs/dune/www/data/2x2/CRS.run2/WarmComissioning/differential_cryoPumpEnabled/"
+#DATASET9_NAME="Cryo_Pump_Enabled_But_Off_Warm"
+#DATE9="2025_08_29"
+#DICT9_NAME="channel_dicts/29August2025_Pedestal_Mod0123_Cryo_Pump_Enabled_NOT_On_2025_08_29_FULL_channel_dict.json"
 
-DATADIR10="/global/cfs/cdirs/dune/www/data/2x2/CRS.run2/WarmComissioning/differential_vacuumPump/"
-DATASET10_NAME="Cryocooler_Jacket_Vacuum_Pump_On_Warm"
-DATE10="2025_08_29"
-DICT10_NAME="channel_dicts/29August2025_Pedestal_Mod0123_Vacuum_Pump_On_2025_08_29_FULL_channel_dict.json"
+#DATADIR10="/global/cfs/cdirs/dune/www/data/2x2/CRS.run2/WarmComissioning/differential_vacuumPump/"
+#DATASET10_NAME="Cryocooler_Jacket_Vacuum_Pump_On_Warm"
+#DATE10="2025_08_29"
+#DICT10_NAME="channel_dicts/29August2025_Pedestal_Mod0123_Vacuum_Pump_On_2025_08_29_FULL_channel_dict.json"
 
-DATADIR11="/global/cfs/cdirs/dune/www/data/2x2/CRS.run2/WarmComissioning/differential_PMalloff/"
-DATASET11_NAME="Purity_Monitor_All_Off_Warm"
-DATE11="2025_09_02"
-DICT11_NAME="channel_dicts/02September2025_Pedestal_Mod0123_PM_AllOff_2025_09_02_FULL_channel_dict.json"
+#DATADIR11="/global/cfs/cdirs/dune/www/data/2x2/CRS.run2/WarmComissioning/differential_PMalloff/"
+#DATASET11_NAME="Purity_Monitor_All_Off_Warm"
+#DATE11="2025_09_02"
+#DICT11_NAME="channel_dicts/02September2025_Pedestal_Mod0123_PM_AllOff_2025_09_02_FULL_channel_dict.json"
 
 YAML_MOD0='/global/cfs/cdirs/dune/users/ehinkle/nd_prototypes_ana/2x2_charge_commission/August2025/geometry/multi_tile_layout-2.3.16_mod0_swap_T8T4T7.yaml'
 YAML_MOD1='/global/cfs/cdirs/dune/users/ehinkle/nd_prototypes_ana/2x2_charge_commission/August2025/geometry/multi_tile_layout-2.3.16_mod1_noswap.yaml'
@@ -77,14 +91,15 @@ module load python
 #    python August2025_Charge_Commission_make_channel_dictionary.py -dir ${DATADIR2} -n ${DATASET2_NAME} -d ${DATE2}
 #fi
 
-python August2025_Charge_Commission_make_pedestal_plots.py -cd ${DICT1_NAME} ${DICT11_NAME} \
-                                                           -n ${DATASET1_NAME} ${DATASET11_NAME} \
-                                                           -d ${DATE1} ${DATE11} \
+python August2025_Charge_Commission_make_pedestal_plots.py -cd ${DICT1_NAME} ${DICT2_NAME} \
+                                                           -n ${DATASET1_NAME} ${DATASET2_NAME} \
+                                                           -d ${DATE1} ${DATE2} \
                                                            -idx ${NOM_DSET_IDX} \
                                                            -l ${YAML_MOD0} ${YAML_MOD1} ${YAML_MOD2} ${YAML_MOD3} \
                                                            -mo 0 1 2 3 \
                                                            -mm ${MAX_MEAN} \
                                                            -ms ${MAX_STD}
+
 
 # SINGLE DATASET
 #python August2025_Charge_Commission_make_pedestal_plots.py -cd ${DICT1_NAME} -n ${DATASET1_NAME} -d ${DATE1} -idx ${NOM_DSET_IDX} -l ${YAML_MOD0} ${YAML_MOD1} ${YAML_MOD2} ${YAML_MOD3} -mo 0 1 2 3 -mm ${MAX_MEAN} -ms ${MAX_STD}
